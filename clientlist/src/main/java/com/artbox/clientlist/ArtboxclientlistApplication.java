c40a317680085716409b6d5c3fe936d33591557a
Luan <rafawow2019@gmail.com>
2025-02-03 20:45:07 -0300

    Configuração inicial do projeto

---
+++ clientlist/src/main/java/com/artbox/clientlist/ArtboxclientlistApplication.java
@@ -0,0 +1,13 @@
+package com.artbox.clientlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtboxclientlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtboxclientlistApplication.class, args);
+	}
+
+}
---
+++ clientlist/src/test/java/com/artbox/clientlist/ArtboxclientlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artbox.clientlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtboxclientlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
